b8de7b76e6e16e6971f14bf8b593c9b186d5c8dd
Rodrigo RA <rrareco@gmail.com>
2020-04-08 23:27:48 -0400

    Versao 3

--- src/main/java/com/aulajpa/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/aulajpa/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.aulajpa.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.aulajpa.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
